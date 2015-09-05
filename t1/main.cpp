51975e5bc30524293a30531f492f3bb6c7b9728e
vinthony <vinthony@gmail.com>
2015-09-05 12:40:39 +0800

    modify the work

--- t1/main.cpp
+++ t1/main.cpp
@@ -3,13 +3,24 @@
 #include <GL/glew.h>
 #include <GLUT/GLUT.h>
 
+#include "math_3d.h"
 
 
 void render(){
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    Vector3f Vertices[1];
+    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_POINTS, 0, 1);
+    glEnableVertexAttribArray(0);
     glutSwapBuffers();
-    
     glFlush();
 }
 
@@ -18,8 +29,8 @@ int main (int argc,char ** argv)
     glutInit(&argc, argv);
     glutInitWindowSize(800,600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("windows1");
-//    glutDisplayFunc(void);
+    glutCreateWindow("t2");
+    //    glutDisplayFunc(void);
     GLenum res = glewInit();
     if(res != GLEW_OK)
     {
@@ -27,7 +38,7 @@ int main (int argc,char ** argv)
         return 1;
     }
     glutDisplayFunc(render);
-    
     glutMainLoop();
+    
     
 }
--- t2/main.cpp
+++ t2/main.cpp
@@ -3,24 +3,13 @@
 #include <GL/glew.h>
 #include <GLUT/GLUT.h>
 
-#include "math_3d.h"
 
 
 void render(){
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    Vector3f Vertices[1];
-    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_POINTS, 0, 1);
-    glEnableVertexAttribArray(0);
     glutSwapBuffers();
+    
     glFlush();
 }
 
@@ -29,7 +18,7 @@ int main (int argc,char ** argv)
     glutInit(&argc, argv);
     glutInitWindowSize(800,600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("windows1");
+    glutCreateWindow("t1");
     //    glutDisplayFunc(void);
     GLenum res = glewInit();
     if(res != GLEW_OK)
@@ -38,7 +27,7 @@ int main (int argc,char ** argv)
         return 1;
     }
     glutDisplayFunc(render);
-    glutMainLoop();
     
+    glutMainLoop();
     
 }
